6e38d59a3fe36fa6dc747723eec31c796f22a7f4
Lucas Alves <lucasalvesbs6@gmail.com>
2026-03-30 22:35:47 -0300

    refactor: improve solution for exercise 65

--- exercises/ex065.py
+++ exercises/ex065.py
@@ -4,9 +4,8 @@ userResponse = ''
 while userResponse != 'N':
   num = int(input('Digite um número inteiro: '))
   
-  if (count) == 0 or num < smallerNumber:
+  if count == 0 or num < smallerNumber:
     smallerNumber = num
-  
   
   if (num > largerNumber):
     largerNumber = num
@@ -15,11 +14,16 @@ while userResponse != 'N':
   count += 1
   
   print('Número computado!')
-  userResponse = str(input('\nDeseja continuar? [S/N] ')).strip().upper()
+  
+  userResponse = 'A'
+  
+  while userResponse not in 'SN':
+    userResponse = str(input('\nDeseja continuar? [S/N] ')).strip().upper()[0]
+  
   print('-' * 50)
   
 average = total / count
 
-print(f'\nA média entre todos os valores é {average:.2f}')
+print(f'\nA média entre os {count} valores é {average:.2f}')
 print(f'O maior valor foi {largerNumber}')
 print(f'O menor valor foi {smallerNumber}')  
