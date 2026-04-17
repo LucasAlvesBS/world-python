605eed8eef6e0c176adfe6b85f5c8fede34886d2
Lucas Alves <lucasalvesbs6@gmail.com>
2026-04-16 22:29:07 -0300

    refactor: improve solution for exercise 77

--- exercises/ex077.py
+++ exercises/ex077.py
@@ -13,12 +13,9 @@ words = (
   'futuro',
 )
 
-vowels = ('a', 'e', 'i', 'o', 'u')
-
-for index, word in enumerate(words):
+for word in words:
   print(f'Na palavra {word.upper()} temos', end=' ')
   for letter in word:  
-    for vowel in vowels:    
-      if (vowel == letter):
-        print(letter, end=' ')
+    if (letter.lower() in 'aeiou'):
+      print(letter, end=' ')
   print(end='\n')
